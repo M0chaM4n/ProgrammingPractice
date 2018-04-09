6a2896706814ade7007ee16a4b9bf3f370cc6240
Abel Mathews <amathews707@gmail.com>
2018-04-08 20:29:48 -0400

    Almost finished but still need to stop the balance  command from returning Some() surrounding the balance, and can't input decimals for Deposit

--- Banking.scala
+++ Banking.scala
@@ -1,44 +1,51 @@
 import scala.io.Source
-import java.nio.file.{Paths, Files}
-import java.nio.charset.StandardCharsets
-
-val file = Source.fromFile("log.html").getLines
+import scala.util.control.Breaks._
+import java.io.FileWriter
 
 object Banking
 {
-	def main(args: Array[Strings])
+	def main(args: Array[String])
 	{
-		val command: String
-
-		while(command != Exit)
+		val file = Source.fromFile("log.html").getLines
+		while(true)
 		{
 			println("Please enter in a command (Deposit, Withdraw, Balance, Exit): ")
-			command = Console.readLine
+			val command = scala.io.StdIn.readLine()
 
-			if command.equalsIgnoreCase("Deposit") 
+			if (command.equalsIgnoreCase("Deposit")) 
 			{
+				breakable {while(true){
 				println("Enter Deposit amount: ")
-				val amount = Console.readLine
-				Files.write(Paths.get(file, amount.getBytes(StandardCharsets.UTF_8))
+				val amount = scala.io.StdIn.readLine()
+				if (amount.toInt < 0)
+					println("Invalid amount")
+				else
+					new FileWriter("log.html", true) { write(s"$amount\n"); close }
+					break
+				}}
 			}
 				
-			else if command.equalsIgnoreCase("Withdraw")
+			else if (command.equalsIgnoreCase("Withdraw"))
 			{
 				println("Enter Withdrawal amount: ")
-				val amount = Console.readLine
+				val amount = scala.io.StdIn.readLine()
+				new FileWriter("log.html", true) { write(s"-$amount\n"); close }
 			}
-			else if command.equalsIgnoreCase("Balance")
+
+			else if (command.equalsIgnoreCase("Balance"))
 			{
-				val amount = file.foldLeft(Option.empty[String]) {case (_, line) => Some(line)}
-				println(s"Your balance is $$amount")
+				val amount = file.foldLeft(Option.empty[String]) {case (_, line) => Option(line)}
+				println(s"Your balance is $amount")
 			}
-			else if command.equalsIgnoreCase("Exit")
+
+			else if (command.equalsIgnoreCase("Exit"))
 			{
-				
+				return
 			}
+
 			else
 				println("Command invalid.")
-				
 		}
-	}
+	}		
 }
+
